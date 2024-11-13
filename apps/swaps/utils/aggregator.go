70a19d4d3c242a71dd2cc15bf7a9d9e0820c5f7e
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2024-11-13 07:08:23 +0530

    feat: updated Aggregator contract deployment addresses

--- apps/swaps/utils/aggregator.go
+++ apps/swaps/utils/aggregator.go
@@ -5,8 +5,8 @@ var aggregators = make(map[int64]string)
 // Initialize adds strings to the map for a given number key
 func InitializeContracts() {
 	// Example initialization
-	aggregators[10] = "0x147B8eb97fD247D06C4006D269c90C1908Fb5D54"
-	aggregators[8453] = "0x147B8eb97fD247D06C4006D269c90C1908Fb5D54"
+	aggregators[10] = "0xfA07E4342F482Eed28DCdCB201db5DeC690894A0"
+	aggregators[8453] = "0x6f1bd0c04b02cB27EFdE27F28cbc464150a6d2eD"
 	aggregators[31337] = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9"
 }
 
