90c609030ca8ed4c5960fd96c7a2ff6e2782005c
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2024-11-18 19:28:39 +0530

    feat: added cors policy to all routes

--- server.go
+++ server.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"rampx/backend/apps/ping"
 	"rampx/backend/apps/swaps"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.Use(cors.Default())
 	swaps.Setup(r.Group("/swap"))
 	ping.Setup(r.Group("/ping"))
 
